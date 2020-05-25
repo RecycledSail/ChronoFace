dd191c683d37997ff5c19305334d9e1b13f8e13d
Lim Jun Young <nogurijr@naver.com>
2020-05-25 15:19:00 +0900

    Initial Commit

---
+++ FaceAgingSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FaceAgingSwift
+//
+//  Created by #include on 2020/05/25.
+//  Copyright © 2020 Lim Jun Young. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
